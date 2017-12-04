73637bb357a52952d57d7d75cd529d9c3758613a
mukund <bal.mukund015435@gmail.com>
2017-12-04 21:52:48 +0530

    Starting dbatz development

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+// This component handles the App template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header
+          loading={this.props.loading}
+        />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*eslint-disable import/default */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <h1>Test</h1>, document.getElementById('app')
+);
